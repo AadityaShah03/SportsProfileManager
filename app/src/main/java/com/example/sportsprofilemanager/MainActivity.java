46811c594b65c8f3408e43c1bd8a795377bc6042
Aaditya Shah <ashah203@uottawa.ca>
2022-10-14 09:07:57 -0400

    small ui changes

--- app/src/main/java/com/example/sportsprofilemanager/MainActivity.java
+++ app/src/main/java/com/example/sportsprofilemanager/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OnOpenInGoogleMaps (View view) {
-        EditText teamAddres = (EditText) findViewById(R.id.teamAddressField);
-// Create a Uri from an intent string. Use the result to create an Intent.
+        EditText teamAddres = (EditText) findViewById(R.id.postalCodeEditText);
+        // Create a Uri from an intent string. Use the result to create an Intent.
         Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+teamAddres.getText());
-// Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-// Make the Intent explicit by setting the Google Maps package
+        // Make the Intent explicit by setting the Google Maps package
         mapIntent.setPackage("com.google.android.apps.maps");
-// Attempt to start an activity that can handle the Intent
+        // Attempt to start an activity that can handle the Intent
         startActivity(mapIntent);
     }
 
